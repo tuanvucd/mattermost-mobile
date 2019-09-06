667c981f3bec5de2bf84d5a70184e0abdb3bc763
hidetoshi <hidetoshi.t@gmail.com>
2019-09-06 10:56:33 +0700

    refactor

--- app/mattermost_managed/mattermost-managed.ios.js
+++ app/mattermost_managed/mattermost-managed.ios.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2015-present GChat, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import {NativeModules, NativeEventEmitter} from 'react-native';
 import LocalAuth from 'react-native-local-auth';
